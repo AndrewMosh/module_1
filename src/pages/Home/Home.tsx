aaed03e4867f5784424b4f60d961f5e676b25dfb
AndrewMosh <malinaenglishclub@gmail.com>
2024-11-22 17:14:15 +0300

    map

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import { Layout } from "../../Components/Layout/Layout"
 import { CardSelection } from "../../Components/CardSelection/CardSelection"
 import Features from "@components/Features/Features"
 import ExchangeRates from "@components/ExchangeRates/ExchangeRates"
+import { Map } from "@components/Map/Map"
 export default function Home() {
 	return (
 		<Layout>
 			<CardSelection />
 			<Features />
 			<ExchangeRates />
+			<Map />
 		</Layout>
 	)
 }
