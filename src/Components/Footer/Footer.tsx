2672e4fb3c086c1943a3a6e50091c65b4e35ed04
AndrewMosh <malinaenglishclub@gmail.com>
2024-11-24 11:42:31 +0300

    all data moved to a different file

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,20 +1,22 @@
 import logo from "@assets/svg/logo.svg";
 import "./Footer.scss";
-import { fMenu } from "./menu-footer.utils";
+import { fMenu, contacts, cookiesInfo } from "./menu-footer.utils";
 import { Link } from "react-router-dom";
 
 export const Footer = () => {
-	const email: string = 'info@neoflex.ru'
-	const phone: string = '+7 (495) 984 25 13'
-	const cookies: string = 'We use cookies to personalize our services and improve the user experience of our website. Cookies are small files containing information about previous visits to a website. If you do not want to use cookies, please change your browser settings'
     return (
         <footer className="footer">
             <div className="footer__container">
                 <div className="footer__header">
                     <img src={logo} alt="logo" />
                     <div className="footer__adresses">
-                        <div className="footer__phone">{phone}</div>
-                        <div className="footer__email">{email}</div>
+						{contacts.map((contact) => (
+							<div key={contact.id}>
+								<Link to={contact.link} className={contact.flag === 'email' ? 'footer__email' : 'footer__phone'}>
+									{contact.name}
+								</Link>
+							</div>
+						))}
                     </div>
                 </div>
                 <ul className="footer__list">
@@ -28,7 +30,7 @@ export const Footer = () => {
                 </ul>
                 <hr className="footer__line" />
                 <div className="footer__cookies">
-                    {cookies}
+                    {cookiesInfo}
                 </div>
             </div>
         </footer>
--- src/Components/Footer/menu-footer.utils.ts
+++ src/Components/Footer/menu-footer.utils.ts
@@ -4,6 +4,13 @@ type TMenu = {
 	link: string;
 }
 
+type TContacts = {
+	id: number;
+	name: string;
+	link: string;
+	flag: string;
+}
+
 export const fMenu: TMenu[] = [
 	{ id: 1, name: 'About bank', link: '/' },
 	{ id: 2, name: 'Ask a Question', link: '/' },
@@ -15,6 +22,11 @@ export const fMenu: TMenu[] = [
 	{ id: 8, name: 'Analytics', link: '/' },
 	{ id: 9, name: 'Business and processes', link: '/' },
 	{ id: 10, name: 'Compliance and business ethics', link: '/' },
-
-
 ]
+
+export const contacts: TContacts[] = [
+	{ id: 1, name: '+7 (495) 984 25 13', link: 'tel:+74959842513', flag:'phone' },
+	{ id: 2, name: 'info@neoflex.ru', link: 'mailto:info@neoflex.ru', flag:'email' },
+]
+
+export const cookiesInfo:string= 'We use cookies to personalize our services and improve the user experience of our website. Cookies are small files containing information about previous visits to a website. If you do not want to use cookies, please change your browser settings'
