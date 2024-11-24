22b0f73509913dc70f5294e9277ba09aca4fa34f
AndrewMosh <malinaenglishclub@gmail.com>
2024-11-24 11:33:28 +0300

    cookies to variable

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
 	const email: string = 'info@neoflex.ru'
 	const phone: string = '+7 (495) 984 25 13'
+	const cookies: string = 'We use cookies to personalize our services and improve the user experience of our website. Cookies are small files containing information about previous visits to a website. If you do not want to use cookies, please change your browser settings'
     return (
         <footer className="footer">
             <div className="footer__container">
@@ -27,9 +28,7 @@ export const Footer = () => {
                 </ul>
                 <hr className="footer__line" />
                 <div className="footer__cookies">
-                    We use cookies to personalize our services and improve the user experience of our website. Cookies are small files
-                    containing information about previous visits to a website. If you do not want to use cookies, please change your browser
-                    settings
+                    {cookies}
                 </div>
             </div>
         </footer>
