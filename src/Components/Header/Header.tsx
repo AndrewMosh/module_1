0ef0e1ea59d9169eeaced9fbc6a14c73d7078c19
AndrewMosh <malinaenglishclub@gmail.com>
2024-11-24 20:14:51 +0300

    hover for button , active menu item color added

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,50 +1,56 @@
-import "./Header.scss";
-import { Link } from "react-router-dom";
-import Button from "@components/UI/Button/Button";
-import { menu } from "./menu.utils";
-import useMenuStore from "@store/useMenuStore";
+import './Header.scss';
+import { Link, NavLink } from 'react-router-dom';
+import Button from '@components/UI/Button/Button';
+import { menu } from './menu.utils';
+import useMenuStore from '@store/useMenuStore';
 
 const Header = () => {
-    const { isMenuOpen, toggleMenu, closeMenu } = useMenuStore();
+  const { isMenuOpen, toggleMenu, closeMenu } = useMenuStore();
 
-    return (
-        <header className="header">
-            <div className="header__container">
-                <Link to="/" className="header__logo">NeoBank</Link>
-             
-                <nav className={`header__nav ${isMenuOpen ? "header__nav--open" : ""}`}>
-                    <button
-                        className="header__close"
-                        aria-label="Close Menu"
-                        onClick={closeMenu}
-                    >
-                        ✕
-                    </button>
-                    <ul className="header__list">
-                        {menu.map((item) => (
-                            <li key={item.id} className="header__item">
-                                <Link
-                                    className="header__link"
-                                    to={item.link}
-                                    onClick={closeMenu} 
-                                >
-                                    {item.name}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <Button children="Online Bank" className="header__button" />
-				<button
-                    className="header__menu"
-                    aria-label="Open Menu"
-                    onClick={toggleMenu}
+  return (
+    <header className="header">
+      <div className="header__container">
+        <Link to="/" className="header__logo">
+          NeoBank
+        </Link>
+
+        <nav className={`header__nav ${isMenuOpen ? 'header__nav--open' : ''}`}>
+          <button
+            className="header__close"
+            aria-label="Close Menu"
+            onClick={closeMenu}
+          >
+            ✕
+          </button>
+          <ul className="header__list">
+            {menu.map((item) => (
+              <li key={item.id} className="header__item">
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'header__link header__link--active'
+                      : 'header__link'
+                  }
+                  to={item.link}
+                  onClick={closeMenu}
                 >
-                    ☰
-                </button>
-            </div>
-        </header>
-    );
+                  {item.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Button children="Online Bank" className="header__button" />
+        <button
+          className="header__menu"
+          aria-label="Open Menu"
+          onClick={toggleMenu}
+        >
+          ☰
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/Components/Header/menu.utils.ts
+++ src/Components/Header/menu.utils.ts
@@ -7,7 +7,7 @@ type TMenu = {
 
 export const menu: TMenu[] = [
 	{ id: 1, name: 'Credit card', link: '/credit-card' },
-	{ id: 2, name: 'Product', link: '/' },
-	{ id: 3, name: 'Account', link: '/' },
-	{ id: 4, name: 'Resources', link: '/' },
+	{ id: 2, name: 'Product', link: '/product' },
+	{ id: 3, name: 'Account', link: '/account' },
+	{ id: 4, name: 'Resources', link: '/resources' },
 ];
