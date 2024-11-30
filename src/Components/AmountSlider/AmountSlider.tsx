2b9abb8549d1e07ca17c14d73078ee228333d872
AndrewMosh <malinaenglishclub@gmail.com>
2024-11-30 21:29:28 +0300

    slider vars separated

--- src/Components/AmountSlider/AmountSlider.tsx
+++ src/Components/AmountSlider/AmountSlider.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './AmountSlider.scss';
 import { useSliderStore } from '@store/sliderStore/useSliderStore';
+import { grey_slider, purple_slider } from './slider.consts';
 
 const Slider: React.FC = () => {
   const { value, setValue } = useSliderStore();
@@ -16,7 +17,7 @@ const Slider: React.FC = () => {
     ) as HTMLInputElement;
     if (rangeInput) {
       const percent = ((value - 15000) / (600000 - 15000)) * 100;
-      rangeInput.style.background = `linear-gradient(to right, #5B35D5 ${percent}%, #E2E8F0 ${percent}%)`;
+      rangeInput.style.background = `linear-gradient(to right, ${purple_slider} ${percent}%, ${grey_slider} ${percent}%)`;
     }
   }, [value]);
 
