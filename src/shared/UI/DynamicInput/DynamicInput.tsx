7d43e7eff72ec05e63961dfca087195dd4f68699
AndrewMosh <malinaenglishclub@gmail.com>
2024-12-29 09:30:33 +0300

    Dynamic Input refactored

--- src/shared/UI/DynamicInput/DynamicInput.tsx
+++ src/shared/UI/DynamicInput/DynamicInput.tsx
@@ -17,36 +17,48 @@ export const DynamicInput: React.FC<InputProps> = ({
   className,
   disabled,
 }) => {
-  const handleClassName = (type: string): string => {
+  const handleClassName = (inputType: string): string => {
     if (error) {
-      return `input__wrapper input__${type}--error`;
+      return `input__wrapper input__${inputType}--error`;
     }
     if (success) {
-      return `input__wrapper input__${type}--success`;
+      return `input__wrapper input__${inputType}--success`;
     }
     return `input__wrapper`;
   };
+
+  const renderInputField = () => {
+    switch (type) {
+      case 'text':
+        return (
+          <div className={handleClassName('text')}>
+            <InputText register={register} placeholder={placeholder} />
+          </div>
+        );
+      case 'select':
+        return (
+          <Select
+            register={register}
+            placeholder={placeholder}
+            options={options}
+            disabled={disabled}
+          />
+        );
+      case 'date':
+        return (
+          <div className={handleClassName('date')}>
+            <InputDate register={register} />
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className={`input ${className}`}>
       <Label label={label} required={required} />
-      {type === 'text' && (
-        <div className={handleClassName('text')}>
-          <InputText register={register} placeholder={placeholder} />
-        </div>
-      )}
-      {type === 'select' && (
-        <Select
-          register={register}
-          placeholder={placeholder}
-          options={options}
-          disabled={disabled}
-        />
-      )}
-      {type === 'date' && (
-        <div className={handleClassName('date')}>
-          <InputDate register={register} />
-        </div>
-      )}
+      {renderInputField()}
       {error && <span className="input__error">{error}</span>}
     </div>
   );
